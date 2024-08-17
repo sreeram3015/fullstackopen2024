88f8c5d51cb9deffddef13b199b177e20b0b3de9
Sreeram P <psreeram3015@proton.me>
2024-08-17 16:17:39 +0530

    Refactored Statistics component to conditionally render feedback details

--- part-1/unicafe/src/components/Statistics.jsx
+++ part-1/unicafe/src/components/Statistics.jsx
@@ -11,12 +11,18 @@ const Statistics = ({
   return (
     <div>
       <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {percentagePositive}</p>
+      {total > 0 ? (
+        <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>All: {total}</p>
+          <p>Average: {average}</p>
+          <p>Positive: {percentagePositive}</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
